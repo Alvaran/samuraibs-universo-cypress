21f7ebb98f6a0c9afa53c3fa12bb6270e5a4188c
ANDRE LUIZ SANTOS <27.andreluiz@gmail.com>
2022-06-28 15:42:27 -0300

    meu peimeiro commit com git

---
+++ cypress/integration/signup.spec.js
@@ -0,0 +1,121 @@
+
+import signupPages from '../support/pages/signup'
+
+//import { faker } from '@faker-js/faker' 
+// const email    = faker.internet.email()
+
+describe('cadastro', () => {
+
+    context('quando o usuario é novato', () => {
+        const user = {
+            name: 'André Luizs',
+            email: 'andreteste@teste.com',
+            password: '123456'
+        }
+
+        before(() => {
+            cy.task('removeUser', user.email)
+                .then(function (result) {
+                    console.log(result)
+                })
+        });
+
+        it('deve cadastrar com sucesso', () => {
+            signupPages.go()
+            signupPages.form(user)
+            signupPages.submit()
+            signupPages.toast.shouldHaveText('Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!')
+        });
+    });
+
+    context('não deve cadastrar usuario', () => {
+        const user = {
+            name: 'Pedro Luiz',
+            email: 'pedro@teste.com',
+            password: '123456',
+            is_provider: true
+        }
+
+        before(() => {
+
+            cy.task('removeUser', user.email)
+                .then(function (result) {
+                    console.log(result)
+                })
+
+            cy.request(
+                'POST',
+                'http://localhost:3333/users',
+                user
+            ).then(function (response) {
+                expect(response.status).to.eq(200)
+            })
+        });
+        it('deve exibir e-mail já cadastrado', () => {
+            signupPages.go()
+            signupPages.form(user)
+            signupPages.submit()
+            signupPages.toast.shouldHaveText('Email já cadastrado para outro usuário.')
+        });
+    });
+
+    context('quando o email é inválido', () => {
+        const user = {
+            name: 'carlos bastos',
+            email: 'carlos.uol.com',
+            password: '123456',
+        }
+
+        it('deve exibir mensagem de alerta', () => {
+            signupPages.go()
+            signupPages.form(user)
+            signupPages.submit()
+            signupPages.alertHaveText('Informe um email válido')
+
+        });
+    });
+
+    context('quando a senha é muito curta', () => {
+
+        const passwords = ['1', '2a', 'ab3', 'abc4', 'ab#c5']
+
+        beforeEach(() => {
+            signupPages.go()
+        });
+
+        passwords.forEach(function (p) {
+            it('não deve cadastrar com a senha: ' + p, () => {
+
+                const user = { name: 'Tereza bastoss', email: 'aloisio@hotmail.com', password: p }
+
+                signupPages.form(user)
+                signupPages.submit()
+            })
+        })
+
+        afterEach(function () {
+            signupPages.alertHaveText('Pelo menos 6 caracteres',)
+        })
+    })
+
+    context('quando não preencho nenhum dos campos ', () => {
+
+        const alertMessages = [
+            'Nome é obrigatório',
+            'E-mail é obrigatório',
+            'Senha é obrigatória',
+        ]
+
+        before(function () {
+            signupPages.go()
+            signupPages.submit()
+        })
+        alertMessages.forEach(function (alert) {
+            it('deve exibir ' + alert.toLowerCase(),function() {
+                signupPages.alertHaveText(alert)
+            });
+
+        })
+    });
+})
+
